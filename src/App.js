097afccd614d39b4a35e0fc49cbbc4a7d0836bb5
Jose <jose@support.webartdesign.com.au>
2019-08-25 23:16:22 +0800

    feat: header Component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router-dom'
 //pages
 import HomePage from './pages/homapage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
+import Header from './components/header/header.component'
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
+        <Header/>
         <Switch>
          <Route exact path='/' component={HomePage}/>
          <Route path='/shop' component={ShopPage}/>
