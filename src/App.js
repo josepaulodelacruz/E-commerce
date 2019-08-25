ad8741eddfc0c1faa68a01f6191934368c3e1170
Jose <jose@support.webartdesign.com.au>
2019-08-25 16:33:38 +0800

    feat: react-router-setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
-
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homapage/homepage.component';
 
 import './App.css';
+
+const HatsPage = () => {
+  return(
+      <div>
+        <h1>Hats Page</h1>
+      </div>
+  )
+}
 
 class App extends React.Component {
   render() {
     return (
       <div className='App'>
-        <HomePage />
+        <Switch>
+         <Route exact path='/' component={HomePage}/>
+         <Route path='/shop/hats' component={HatsPage}/>
+        </Switch>
       </div>
     );
   }
